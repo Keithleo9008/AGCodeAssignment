3faa882ff051b83218884063ea159db75eb733d7
keith Leo-Smith <kleo-smith@deloitte.co.za>
2017-07-08 23:00:50 +0200

    commit

---
+++ CodeAssignmentTest/ServiceLayer/TwitterDataServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeAssignment.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAssignment.ServiceLayer.Tests
+{
+    [TestClass()]
+    public class TwitterDataServiceTests
+    {
+        [TestMethod()]
+        public void TwitterDataServiceTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetTweetsByUidTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetTwitterFeedTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetUserListTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
